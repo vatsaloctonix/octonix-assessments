e2539420360b6ea933eec0183c1ff385c561f74e
vatsaloctonix <vatsal.gajjar@octonixconsult.com>
2025-12-30 19:34:29 -0500

    Update page.tsx

--- app/roles/page.tsx
+++ app/roles/page.tsx
@@ -1,12 +1,10 @@
 import { OctonixFrame } from "@/components/ui";
-
 import { Suspense } from "react";
 import RolesClient from "./rolesClient";
-
 export default function RolesPage() {
   return (
     <OctonixFrame>
-      <Suspense fallback={<div className="text-sm text-black/60">Loading...</div>}>
+      <Suspense fallback={<div />}>
         <RolesClient />
       </Suspense>
     </OctonixFrame>
