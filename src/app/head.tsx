6b1c6183fb3b1d36613ff0882d88fc15267b0254
vatsaloctonix <vatsal.gajjar@octonixconsult.com>
2026-01-23 03:51:44 -0500

    Force custom favicon links

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      <link rel="icon" href="/icon.png" type="image/png" sizes="64x64" />
+      <link rel="apple-touch-icon" href="/icon.png" />
+    </>
+  );
+}
